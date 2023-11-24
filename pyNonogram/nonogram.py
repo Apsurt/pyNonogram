2159fb1b5fa313d571ba6a40bd03640003490343
Apsurt <tymon@becella.eu>
2023-11-24 20:22:00 +0100

    Refactor game.py: Remove unused methods and improve code readability and add docstrings

--- pyNonogram/nonogram.py
+++ pyNonogram/nonogram.py
@@ -1,13 +1,40 @@
+#Internal imports
+from nonogram_grid import NonogramGrid
+
+#Built-in imports
+from typing import Optional
+import os
+
+#External imports
+import numpy as np
+
 class Nonogram:
+    """Nonogram class, used to store nonogram data.
+    
+    :param \**kwargs:
+        See below
+    
+    :Keyword Arguments:
+        * *path* (``str``) --
+          Path to nonogram file (.non) or directory with nonogram files
+    """    
     def __init__(self, **kwargs) -> None:
-        self.init_vars()
+        """Creates a new Nonogram object.
+
+        :raises Exception: Invalid path when path is neither file nor directory.
+        """        
         self.path = None
-        
+        self.path_type = None
         if 'path' in kwargs:
             self.path = kwargs['path']
-            self.load(self.path)
-    
-    def init_vars(self) -> None:
+            if os.path.isfile(self.path):
+                self.path_type = 'file'
+            elif os.path.isdir(self.path):
+                self.path_type = 'dir'
+            else:
+                raise Exception('Invalid path')
+        
+        #declare variables
         self.author = None
         self.date = None
         self.picture = None
@@ -17,65 +44,221 @@ class Nonogram:
         self.rows = None
         self.columns = None
         self.solution = None
+        
         self.solved = False
-    
-    def load(self, path: str) -> None:
+        self.is_loaded = False
+        
+        self.grid = None
+        
+    def load(self, path: Optional[str]) -> None:
+        """Loads a nonogram from a file.
+
+        :param path: Path to nonogram file (.non)
+        :type path: Optional[str]
+        :raises Exception: Invalid path when path is neither file nor directory.
+        :raises Exception: When path and self.path are None.
+        :raises Exception: Invalid path type when path is not a file.
+        :raises Exception: Invalid file format when file does not end with (.non)
+        :raises Exception: Invalid file format when file does not have 9 lines.
+        """        
+        if path is not None:
+            self.path = path
+            if os.path.isfile(self.path):
+                self.path_type = 'file'
+            elif os.path.isdir(self.path):
+                self.path_type = 'dir'
+            else:
+                raise Exception('Invalid path')
+        
+        if self.path is None:
+            raise Exception('Path not specified')
+
+        if self.path_type != 'file':
+            raise Exception('Invalid path type. (Expected file got {})'.format(self.path_type))
+
+        if not self.path.endswith('.non'):
+            raise Exception('Invalid file format (Expected .non got {})'.format(self.path.split('.')[-1]))
+
+        #reads file
         with open(path, 'r') as f:
             data = f.readlines()
+
+        #checks if file has 9 lines (author, date, picture, difficulty, width, height, rows, columns, solution)
+        #this is the structure of a nonogram file (.non)
         if len(data) != 9:
             raise Exception('Invalid file format')
         
+        #first line: author
         self.author = data[0].split(':')[1].strip('\n')
+        #second line: date
         self.date = data[1].split(':')[1].strip('\n')
+        #third line: picture rating
         self.picture = int(data[2].split(':')[1].strip('\n'))
+        #fourth line: difficulty
         self.difficulty = int(data[3].split(':')[1].strip('\n'))
+        #fifth line: width
         self.width = int(data[4].split(':')[1].strip('\n'))
+        #sixth line: height
         self.height = int(data[5].split(':')[1].strip('\n'))
-
+        
+        #seventh line: row hints
         self.rows = data[6].split(':')[1].strip('\n').split(' ')
         for idx in range(len(self.rows)):
             self.rows[idx] = self.rows[idx].split(',')
             self.rows[idx] = list(map(int, self.rows[idx]))
-
+        
+        #eighth line: column hints
         self.columns = data[7].split(':')[1].strip('\n').split(' ')
         for idx in range(len(self.columns)):
             self.columns[idx] = self.columns[idx].split(',')
             self.columns[idx] = list(map(int, self.columns[idx]))
-
+        
+        #ninth line: solution
         self.solution = data[8].split(':')[1].strip('\n')
         self.solution = list(map(''.join, zip(*[iter(self.solution)]*self.width)))
         for idx in range(len(self.solution)):
             self.solution[idx] = list(map(int, self.solution[idx]))
+        #if solution is empty, set it to None
         if len(self.solution) == 0:
             self.solution = None
             self.solved = False
+        
+        self.is_loaded = True
+        
+        #load grid
+        self.load_grid()
     
-    def load_random(self, size: int) -> None:
-        pass
+    def load_random(self, path: Optional[str]) -> None:
+        """Loads a random nonogram from a directory.
+
+        :param path: Path to directory with nonogram files (.non)
+        :type path: Optional[str]
+        :raises Exception: Invalid path when path is neither file nor directory.
+        :raises Exception: When path and self.path are None.
+        :raises Exception: Invalid path type when path is not a directory.
+        """        
+        if path is not None:
+            self.path = path
+            if os.path.isfile(self.path):
+                self.path_type = 'file'
+            elif os.path.isdir(self.path):
+                self.path_type = 'dir'
+            else:
+                raise Exception('Invalid path')
+        
+        if self.path is None:
+            raise Exception('Path not specified')
+
+        if self.path_type != 'dir':
+            raise Exception('Invalid path type. (Expected dir got {})'.format(self.path_type))
+        
+        #gets random file from directory
+        files = os.listdir(self.path)
+        _file = np.random.choice(files)
+        
+        #loads it
+        self.load(os.path.join(self.path, _file))
     
-    def print_solution(self) -> str:
-        if self.solution is None:
-            raise RuntimeError('Nonogram is not solved')
-        result = ''
+    def load_grid(self):
+        """Loads the nonogram grid object.
+
+        :raises Exception: Nonogram not loaded.
+        """        
+        if not self.is_loaded:
+            raise Exception('Nonogram not loaded')
+        self.grid = NonogramGrid((self.height, self.width))
+    
+    def save_solution(self):
+        """Saves current grid state as solution in nonogram file at self.path.
+        
+        :raises Exception: When path and self.path are None.
+        :raises Exception: Invalid path type when path is not a file.
+        :raises RuntimeError: _description_
+        """
+        if self.path is None:
+            raise Exception('Path not specified')
+
+        if self.path_type != 'file':
+            raise Exception('Invalid path type. (Expected file got {})'.format(self.path_type))
+        #iterate over grid and save solution
+        self.solution = ""
         for y in range(self.height):
             for x in range(self.width):
-                result += u"\u2588"*2 if self.solution[y][x] == 1 else '  '
-            result += '\n'
-        print(result[:-1])
+                if self.get_cell(x,y) == 0:
+                    raise RuntimeError('Nonogram is not solved')
+                elif self.get_cell(x,y) == 1:
+                    self.solution += "1"
+                elif self.get_cell(x,y) == -1:
+                    self.solution += "0"
+        if self.solved == False:
+            self.solved = True
+            with open(self.path, 'a') as f:
+                f.write(f"{self.solution}")
+    
+    def load_solution(self):
+        """Loads solution from nonogram file at self.path.
 
-def main():
-    non = Nonogram(path='src/nonograms/67723.non')
-    print(non.author)
-    print(non.date)
-    print(non.picture)
-    print(non.difficulty)
-    print(non.width)
-    print(non.height)
-    print(non.rows)
-    print(non.columns)
-    print(non.solved)
-    if non.solved:
-        print(non.solution)
+        :raises Exception: Nonogram not loaded.
+        :raises Exception: Nonogram is not solved or has no solution.
+        """        
+        if not self.is_loaded:
+            raise Exception('Nonogram not loaded')
+        if self.solution is None or self.solved == False:
+            raise Exception('Nonogram has no solution')
+        #iterate over solution and load it into grid
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.solution[y][x] == 1:
+                    self.grid.set_cell(x, y, 1)
+                elif self.solution[y][x] == 0:
+                    self.grid.set_cell(x, y, -1)
 
-if __name__ == '__main__':
-    main()
+    def print(self) -> None:
+        """Prints the nonogram to the console.
+
+        :raises Exception: Nonogram not loaded.
+        """        
+        if not self.is_loaded:
+            raise Exception('Nonogram not loaded')
+        if self.grid is None:
+            self.load_grid()
+        
+        #get max row and column length
+        max_row_len = max(list(map(len, self.rows)))
+        max_col_len = max(list(map(len, self.columns)))
+        
+        #get spacing between columns
+        col_space = len(str(np.max(list(map(np.max, self.columns)))))+1
+        
+        #print path
+        print(self.path)
+            
+        for i in range(max_col_len):
+            #print empty space in left upper corner
+            print('   '*max_row_len, end='')
+            
+            #print column hints
+            i = max_col_len - i - 1
+            col_str = ''
+            for x in range(self.width):
+                if i < len(self.columns[x]):
+                    col_str += str(self.columns[x][i]).rjust(col_space)
+                else:
+                    col_str += ' '*col_space
+            print(col_str)
+        
+        for y in range(self.height):
+            #print row hints
+            strs = list(map(lambda num: str(num).rjust(2), self.rows[y]))
+            while len(strs) < max_row_len:
+                strs.insert(0, '  ')
+            print(' '.join(strs), end=' ')
+            
+            #print board
+            row = self.grid.get_row(y)
+            row_str = ''.join(map(str, row))
+            row_str = row_str.replace('0', '-'*col_space)
+            row_str = row_str.replace('-1', ' '*col_space)
+            row_str = row_str.replace('1', u'\u2588'*col_space)
+            
+            print(row_str)
---
+++ pyNonogram/nonogram_grid.py
@@ -0,0 +1,205 @@
+#Built-in imports
+from typing import Tuple, List
+
+#External imports
+import numpy as np
+
+class NonogramGrid(np.ndarray):
+    """NonogramGrid class, used to store nonogram grid data.
+
+    .. notes:: Inherits from :class:`numpy.ndarray`
+    """    
+    def __new__(cls, *args, **kwargs) -> np.ndarray:
+        """Creates a new NonogramGrid object.
+
+        :return: Returns 2D array of type np.int8
+        :rtype: np.ndarray
+        """
+        #set dtype to np.int8
+        kwargs['dtype'] = np.int8
+        return super().__new__(cls, *args, **kwargs)
+    
+    def __init__(self, shape: Tuple[int, int], *args, **kwargs) -> None:
+        """Creates a new NonogramGrid object.
+
+        :param shape: (height, width)
+        :type shape: Tuple[int, int]
+        """        
+        args = (shape, *args)
+        #initialize with zeros
+        self.fill(0)
+    
+    def set_cell(self, x: int, y: int, value: int) -> None:
+        """Sets a cell value.
+
+        :param x: x coordinate
+        :type x: int
+        :param y: y coordinate
+        :type y: int
+        :param value: value to set (0, 1 or -1)
+        :type value: int
+        """
+        #Swappped x and y because of numpy array indexing, y is row, x is column. Holds for all methods.
+        self[y,x] = value
+    
+    def get_cell(self, x: int, y: int) -> int:
+        """Returns a cell value.
+
+        :param x: x coordinate
+        :type x: int
+        :param y: y coordinate
+        :type y: int
+        :return: cell value (0, 1 or -1)
+        :rtype: int
+        """        
+        return self[y,x]
+    
+    def get_row(self, y: int) -> np.ndarray:
+        """Returns cell values in a row.
+
+        :param y: y coordinate of row
+        :type y: int
+        :return: row
+        :rtype: np.ndarray
+        """        
+        return self[y]
+    
+    def get_col(self, x: int) -> np.ndarray:
+        """Returns cell values in a column.
+
+        :param x: x coordinate of column
+        :type x: int
+        :return: column
+        :rtype: np.ndarray
+        """        
+        return self[:,x]
+    
+    def fill_row(self, y: int, value: int, with_overwritting: bool = True) -> None:
+        """Fills a row with a value.
+
+        :param y: y coordinate of row
+        :type y: int
+        :param value: value to fill with (0, 1 or -1)
+        :type value: int
+        :param with_overwritting: decides if existing non-zero values should be overwritten, defaults to True
+        :type with_overwritting: bool, optional
+        """        
+        for x in range(self.shape[1]):
+            if with_overwritting or self[y,x] == 0:
+                self.set_cell(x, y, value)
+    
+    def fill_col(self, x: int, value: int, with_overwritting: bool = True) -> None:
+        """Fills a column with a value.
+
+        :param x: x coordinate of column
+        :type x: int
+        :param value: value to fill with (0, 1 or -1)
+        :type value: int
+        :param with_overwritting: decides if existing non-zero values should be overwritten, defaults to True
+        :type with_overwritting: bool, optional
+        """        
+        for y in range(self.shape[0]):
+            if with_overwritting or self[y,x] == 0:
+                self.set_cell(x, y, value)
+    
+    def get_row_segments(self, y: int) -> List[int]:
+        """Returns lengths of segments of 1s in a row.
+
+        :param y: y coordinate of row
+        :type y: int
+        :return: Lengths of segments of 1s in a row.
+        :rtype: List[int]
+        """        
+        row = self.get_row(y)
+        #replace -1 with 0 as they are the same in this case
+        row = np.maximum(row,0)
+        row_str = ''.join(map(str, row))
+        #splits into segments of 1s
+        row_str = row_str.split('0')
+        #removes empty segments
+        segments = list(filter(lambda x: len(x) > 0, row_str))
+        #maps segments to their lengths
+        segments = list(map(len, segments))
+        return segments
+    
+    def get_col_segments(self, x: int) -> List[int]:
+        """Returns lengths of segments of 1s in a column.
+
+        :param x: x coordinate of column
+        :type x: int
+        :return: Lengths of segments of 1s in a column.
+        :rtype: List[int]
+        """        
+        col = self.get_col(x)
+        col = np.maximum(col,0)
+        col_str = ''.join(map(str, col))
+        col_str = col_str.split('0')
+        segments = list(filter(lambda x: len(x) > 0, col_str))
+        segments = list(map(len, segments))
+        return segments
+    
+    def check_row(self, y: int) -> bool:
+        """Checks if a row is solved.
+
+        :param y: y coordinate of row
+        :type y: int
+        :return: True if row is solved, False otherwise.
+        :rtype: bool
+        """        
+        segments = self.get_row_segments(y)
+        #if number of segments is not equal to number of hints, row is not solved
+        if len(segments) != len(self.rows[y]):
+            return False
+        #if all segments are equal to their corresponding hint, row is solved
+        if np.all(segments == self.rows[y]):
+            return True
+        #otherwise row is not solved
+        return False
+    
+    def check_col(self, x: int) -> bool:
+        """Checks if a column is solved.
+
+        :param x: x coordinate of column
+        :type x: int
+        :return: True if column is solved, False otherwise.
+        :rtype: bool
+        """        
+        segments = self.get_col_segments(x)
+        if len(segments) != len(self.columns[x]):
+            return False
+        if np.all(segments == self.columns[x]):
+            return True
+        return False
+    
+    def check_all(self) -> bool:
+        """Checks if all rows and columns are solved.
+
+        :return: True if all rows and columns are solved, False otherwise.
+        :rtype: bool
+        """
+        #if any row or column is not solved, nonogram is not solved
+        for y in range(self.shape[0]):
+            res = self.check_row(y)
+            if not res:
+                return False
+        for x in range(self.shape[1]):
+            res = self.check_col(x)
+            if not res:
+                return False
+        return True
+    
+    def is_solved(self) -> bool:
+        """Is nonogram solved.
+
+        :return: True if nonogram is solved, False otherwise.
+        :rtype: bool
+        """        
+        return self.check_all()
+    
+    def __str__(self) -> str:
+        """Returns string representation of NonogramGrid.
+
+        :return: np.narray.__str__()
+        :rtype: str
+        """        
+        return super().__str__()
