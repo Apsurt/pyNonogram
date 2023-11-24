fe21c02292a624791f91195136671b4b885e4240
Apsurt <tymon@becella.eu>
2023-11-24 22:02:26 +0100

    Refactor Display class to use type hints and add docstrings

--- pyNonogram/display.py
+++ pyNonogram/display.py
@@ -1,11 +1,23 @@
+#Internal imports
+from nonogram import NonogramType
+
 #External imports
 import matplotlib.pyplot as plt
 
 class Display:
-    def __init__(self, nonogram):
+    """Display class, used to display nonogram grid data.
+    """    
+    def __init__(self, nonogram: NonogramType) -> None:
+        """Creates a new Display object.
+
+        :param nonogram: Nonogram class object
+        :type nonogram: NonogramType
+        """        
         self.nonogram = nonogram
     
-    def generate(self):
+    def generate(self) -> None:
+        """Generates a matplotlib figure.
+        """        
         self.fig = plt.figure()
         self.fig.set_size_inches(self.nonogram.width/5, self.nonogram.height/5)
         self.ax = plt.Axes(self.fig, [0., 0., 1., 1.])
@@ -14,10 +26,17 @@ class Display:
         plt.set_cmap('binary')
         self.ax.imshow(self.nonogram.grid)
     
-    def show(self):
+    def show(self) -> None:
+        """Shows the matplotlib figure.
+        """        
         self.generate()
         plt.show()
     
-    def save(self, path):
+    def save(self, path: str) -> None:
+        """Saves the matplotlib figure to png.
+
+        :param path: path to save to
+        :type path: str
+        """        
         self.generate()
         plt.savefig(path)
