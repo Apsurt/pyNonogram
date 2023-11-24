d77b566a8ba98d03b971ff82f9513cdfac7ccd1c
Apsurt <tymon@becella.eu>
2023-11-24 20:11:22 +0100

    Update copyright information in LICENSE file and add pyNonogram package

---
+++ pyNonogram/___init___.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+pyNonogram
+==========
+
+Description
+
+:copyright:     (c) 2023 Tymon Becella
+:license:       MIT
+"""
+
+__title__ = 'pyNonogram'
+__author__ = 'Tymon Becella'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 Tymon Becella'
+__version__ = '0.7.0'
+
+from .browser import *
+from .display import *
+from .errors import *
+from .nonogram_grid import *
+from .nonogram import *
+from .scraper import *
+from .solver import *
