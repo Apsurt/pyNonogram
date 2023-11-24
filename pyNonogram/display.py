fb8a5d2c4aa85ea39947ca3fc85a8d51c1ff64cd
Apsurt <tymon@becella.eu>
2023-11-24 21:23:45 +0100

    Refactor Display class to use nonogram instead of game

--- pyNonogram/display.py
+++ pyNonogram/display.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
 class Display:
-    def __init__(self, game):
-        self.game = game
+    def __init__(self, nonogram):
+        self.nonogram = nonogram
     
     def generate(self):
         self.fig = plt.figure()
-        self.fig.set_size_inches(self.game.width/5, self.game.height/5)
+        self.fig.set_size_inches(self.nonogram.width/5, self.nonogram.height/5)
         self.ax = plt.Axes(self.fig, [0., 0., 1., 1.])
         self.ax.set_axis_off()
         self.fig.add_axes(self.ax)
         plt.set_cmap('binary')
-        self.ax.imshow(self.game.grid)
+        self.ax.imshow(self.nonogram.grid)
     
     def show(self):
         self.generate()
