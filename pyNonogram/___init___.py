eef45b9a77df16d5589a39f343c7034058e95ced
Apsurt <tymon@becella.eu>
2023-11-25 22:00:38 +0100

    Update version numbers in __init__.py and setup.py

--- pyNonogram/___init___.py
+++ pyNonogram/___init___.py
@@ -14,7 +14,7 @@ __title__ = 'pyNonogram'
 __author__ = 'Tymon Becella'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023 Tymon Becella'
-__version__ = '0.1.0'
+__version__ = '0.0.1'
 
 from .errors import *
 from .nonogram_grid import *
--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pyNonogram',
-      version='0.1.0',
+      version='0.0.1',
       description='',
       long_description=long_description,
       url='https://github.com/Apsurt/pyNonogram',
