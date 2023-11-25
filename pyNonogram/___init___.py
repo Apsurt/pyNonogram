e50f1edc56352f89932c357bbc80e747124fcb4e
Apsurt <tymon@becella.eu>
2023-11-25 18:48:19 +0100

    Remove unnecessary imports and files

--- pyNonogram/___init___.py
+++ pyNonogram/___init___.py
@@ -16,10 +16,6 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2023 Tymon Becella'
 __version__ = '0.7.0'
 
-from .browser import *
-from .display import *
 from .errors import *
 from .nonogram_grid import *
 from .nonogram import *
-from .scraper import *
-from .solver import *
