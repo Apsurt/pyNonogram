9e4761c191b52d065fe6741be1cc096fa351ba88
Apsurt <tymon@becella.eu>
2024-01-01 19:37:11 +0100

    v0.2.0

--- pyNonogram/errors.py
+++ pyNonogram/errors.py
@@ -1,1 +1,42 @@
-#tba
+# -*- coding: utf-8 -*-
+
+
+class PathException(Exception):
+    """
+    Exception that's thrown when the path is invalid
+    """
+    pass
+
+
+class NonogramException(Exception):
+    """
+    Base exception for all nonogram exceptions
+    """
+    pass
+
+
+class LoadingException(NonogramException):
+    """
+    Exception that's thrown when errors occur while loading a nonogram.
+    """
+    pass
+
+
+class NotLoaded(NonogramException):
+    """
+    Exception that's thrown when a nonogram is not loaded.
+    """
+    pass
+
+class NotSolved(NonogramException):
+    """
+    Exception that's thrown when a nonogram is not solved.
+    """
+    pass
+
+class UnknownFormat(Exception):
+    """
+    Exception thrown when trying to export to a unknown format.
+    Supported formats: .non
+    """
+    pass
