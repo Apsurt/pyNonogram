d9b317c4db2f6e9437ece8c975077976e2e14ace
Apsurt <tymon@becella.eu>
2024-01-01 19:54:15 +0100

    Refactor error classes in pyNonogram

--- pyNonogram/errors.py
+++ pyNonogram/errors.py
@@ -2,41 +2,34 @@
 
 
 class PathException(Exception):
-    """
-    Exception that's thrown when the path is invalid
+    """Exception that's thrown when the path is invalid
     """
     pass
 
 
 class NonogramException(Exception):
-    """
-    Base exception for all nonogram exceptions
+    """Base exception for all nonogram exceptions
     """
     pass
 
 
 class LoadingException(NonogramException):
-    """
-    Exception that's thrown when errors occur while loading a nonogram.
+    """Exception that's thrown when errors occur while loading a nonogram.
     """
     pass
 
 
 class NotLoaded(NonogramException):
-    """
-    Exception that's thrown when a nonogram is not loaded.
+    """Exception that's thrown when a nonogram is not loaded.
     """
     pass
 
 class NotSolved(NonogramException):
-    """
-    Exception that's thrown when a nonogram is not solved.
+    """Exception that's thrown when a nonogram is not solved.
     """
     pass
 
 class UnknownFormat(Exception):
-    """
-    Exception thrown when trying to export to a unknown format.
-    Supported formats: .non
+    """Exception thrown when trying to export to a unknown format. Supported formats: .non
     """
     pass
